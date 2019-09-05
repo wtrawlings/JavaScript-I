fe5b05b8831b03071b35b971a517425b86885e31
Warren Rawlings <rawlings.warren@gmail.com>
2019-09-04 20:43:28 -0400

    completed arrow function conversion

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,21 +4,29 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+let myFunction = () => console.log("Function was invoked!");
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+let anotherFunction = (param) => param;
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => param1 + param2;
+add(1, 2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => param1 - param2;
+subtract(1, 2);
 
 
 // Stretch
@@ -28,3 +36,6 @@
 //   return num * 3;
 // });
 // console.log(triple);
+exampleArray = [1, 2, 3, 4];
+const triple = exampleArray.map((num) => num * 3);
+console.log(triple);
